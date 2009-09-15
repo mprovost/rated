b4ed99e0d1664a6339cabaf51b5efa9c0cba80e0
Matt Provost <mprovost@wetafx.co.nz>
2009-09-15 12:21:05 +1200

    add __db_check_oids_table

--- src/rateddbi.h
+++ src/rateddbi.h
@@ -13,6 +13,7 @@ int __db_commit();
 int __db_insert(const char *table_esc, unsigned long iid, unsigned long long counter, double rate);
 int __db_lookup_oid(char *, char *, unsigned long *);
 char *__db_escape_string(const char *input);
+int __db_check_oids_table();
 
 /*
  * we have to jump through some hoops when we load the functions from a library
@@ -25,3 +26,4 @@ int (*db_commit)();
 int (*db_insert)(const char *table, unsigned long iid, unsigned long long counter, double rate);
 int (*db_lookup_oid)(char *host, char *oid, unsigned long *iid);
 char *(*db_escape_string)(const char *input);
+int (*db_check_oids_table)();
